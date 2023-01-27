9cf7886ec48627e34f3effc150fe2a0643bb6bba
unknown <hualbert@rocketmail.com>
2023-01-26 21:07:48 -0800

    finally made the app to retrieve the list correctly

--- controller/movieController.js
+++ controller/movieController.js
@@ -10,23 +10,52 @@ const MovieInstance = require('../model/MovieInstance')
 const MovieData = require('../data/movie.json')
 
 exports.MovieList = (req, res, next) => {
-    Movie.find({})
-        .sort({ title: 1 })
-        .populate("director")
-        .populate("actor")
-        .populate("genre")
-        .populate("movieinstance")
-        .exec(function (err, result){
+    async.parallel(
+        {
+            MovieList(callback) {
+                Movie.find({})
+                    .sort({ title: 1 })
+                    .populate("actors")
+                    .populate("genres")
+                    .exec(callback)
+            },
+            DirectorList(callback) {
+                Director.find({})
+                .exec(callback)
+            }
+        },
+        (err, result) => {
             if (err) {
                 return next(err)
             }
-            if (result != null) { 
+            console.log("Director: ", result.DirectorList[0]._id)
+            if (result != null) {
                 res.render('index', {
                     title: "Movies",
-                    movie_list: result,
+                    movie_list: result.MovieList,
+                    director_list: result.DirectorList, 
                 })
             }
-        })
+        }
+    )
+    //Movie.find({})
+    //    .sort({ title: 1 })
+    //    .populate("director")
+    //    .populate("actors")
+    //    .populate("genres")
+    //    .exec(function (err, result){
+    //        if (err) {
+    //            return next(err)
+    //        }
+    //        console.log("film: ", result[0].title)
+    //        console.log("director: ", result[0].director)
+    //        if (result != null) { 
+    //            res.render('index', {
+    //                title: "Movies",
+    //                movie_list: result,
+    //            })
+    //        }
+    //    })
 }
 
 exports.MovieDetail = (req, res, next) => { }
