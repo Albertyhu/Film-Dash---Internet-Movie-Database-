763bc4a000fe0c114117a042f9dc9da87e68f846
unknown <hualbert@rocketmail.com>
2023-02-07 18:36:03 -0800

    created contact form

---
+++ controller/contactController.js
@@ -0,0 +1,19 @@
+
+const Genres = require('../model/Genres')
+
+exports.ContactForm_Get = (req, res, next) => {
+    Genres.find({})
+        .exec((err, result) => {
+            if (err) {
+                return next(err)
+            }
+            res.render('contact', {
+                title: "What's on your mind? Express yourself through email.",
+                genre_list: result,
+                logo: "../images/FilmDashLogo.png",
+                burgerMenu: "../../icon/hamburger_menu_white.png",
+            })
+        })
+
+}
+exports.ContactForm_Post = [];  
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require("express");
 var router = express.Router();
+const ContactController = require('../controller/contactController');
 
 /* GET home page. */
 router.get("/", (req, res) => {
   res.redirect("/catalog");
 });
+
+
+router.get('/contact', ContactController.ContactForm_Get);
+router.post('/contact', ContactController.ContactForm_Post); 
 
 module.exports = router;
