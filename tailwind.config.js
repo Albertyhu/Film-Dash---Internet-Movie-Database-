5fa4b15b7b3994a93c94cc4a274e6571dddd02f1
unknown <hualbert@rocketmail.com>
2023-02-24 17:26:52 -0800

    fixed small formatting issue with tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./views/**/*.{html, js, ejs}"],
+  content: ["./views/**/*.{html,js,ejs}"],
   theme: {
     extend: {
       colors: {
         "Panel-BG": "rgba(199, 255, 218, 1)",
         "CancelButton-Color": "#E8E1EF",
-        NewMsgButtonColor: "#C4F4C7",
+        "NewMsgButtonColor": "#C4F4C7",
           },
       screens: {
         'xm': { 'min': '360px' },
