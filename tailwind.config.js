ef9f0d97bb8a51413bcffce85b94fe24e8e43e94
unknown <hualbert@rocketmail.com>
2023-02-24 17:00:23 -0800

    fixed responsive issue in mobile devices

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,15 @@ module.exports = {
         "Panel-BG": "rgba(199, 255, 218, 1)",
         "CancelButton-Color": "#E8E1EF",
         NewMsgButtonColor: "#C4F4C7",
-      },
+          },
+      screens: {
+        'xm': { 'min': '360px' },
+        'sm': { 'min': '640px' },
+        'md': { 'min': '768px' },
+        'lg': { 'min': '1024px' },
+        'xl': { 'min': '1280px' },
+        '2xl': { 'min': '1536px' }
+      }
     },
   },
   plugins: [
